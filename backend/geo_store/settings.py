d9e3d146ecac289ff420b0c3c368746edaa107df
Michal Wesolowski <73594009+wesolowskimichal@users.noreply.github.com>
2025-02-11 18:29:30 +0100

    [#6] Docker & docker-compose (#9)
    * Write a Dockerfile for the Django app.
    * Create docker-compose.yml with a service for web and db.
    * Use postgres

--- backend/geo_store/settings.py
+++ backend/geo_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "geo_store.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "geo_store_db"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": 5432,
     }
 }
 
