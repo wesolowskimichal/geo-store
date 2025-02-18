c901924a54a637089482b363ad34637f07fe410d
Michal Wesolowski <michal.wesolov@gmail.com>
2025-02-19 00:09:57 +0100

    create app

---
+++ frontend/src/app.tsx
@@ -0,0 +1,16 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({
+  routeTree,
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
