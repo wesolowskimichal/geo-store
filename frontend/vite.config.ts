2fbe8cf221a8dbb30d74a4e9b434f6a04a6b125e
Michal Wesolowski <73594009+wesolowskimichal@users.noreply.github.com>
2025-02-19 20:54:41 +0100

    [NT] fix docker fe setup (#31)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,20 @@
 import path from 'path';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true,
     },
   },
 });
